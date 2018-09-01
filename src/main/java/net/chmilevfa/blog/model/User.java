128d02ac531d589563c4d58d1bd85c8549e65761
chmilevfa <chmilev.fedor@gmail.com>
2018-09-01 21:14:06 +0200

    Model for User added

---
+++ src/main/java/net/chmilevfa/blog/model/User.java
@@ -0,0 +1,64 @@
+package net.chmilevfa.blog.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * Describes entity for any registered user.
+ *
+ * @author chmilevfa
+ * @since 26.08.18
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    @Length(min = 5, message = "*Your username must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your username")
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    @Length(min = 5, message = "*Your password must have at least 5 characters")
+    @NotEmpty(message = "*Please provide your password")
+    @JsonIgnore
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Please provide an email")
+    private String email;
+
+    @Column(name = "name")
+    @NotEmpty(message = "*Please provide your name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
