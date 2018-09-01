226eec45fd53fe08ac843bff5a71694f90d51123
chmilevfa <chmilev.fedor@gmail.com>
2018-09-01 21:16:29 +0200

    spring-boot-starter-security dependency and basic configuration added

---
+++ src/main/java/net/chmilevfa/blog/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package net.chmilevfa.blog.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration for Spring Security.
+ *
+ * @author chmilevfa
+ * @since 26.08.18
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/", "/error", "/registration").permitAll()
+                .antMatchers().authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
